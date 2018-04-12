26477f5c3713e9e983d11c133eaeb1bb0c6ec4bc
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2018-04-11 21:42:31 -0300

    Add files via upload

---
+++ exercicios_introducao.hs
@@ -0,0 +1,67 @@
+{-
+- Usando os predicados not,and e or prontos de Haskell, implemente os predicados (funcoes) xor (or exclusivo),
+- impl (implicacao A => B é equivalente a (not A or B)) e equiv (A <=> B é definido como A => B and B => A)
+- Procure usar casamento de padroes e reutilizar as funcoes.
+-}
+
+xor::Bool -> Bool -> Bool 
+xor True True = False
+xor False False = False
+xor _ _ = True 
+
+
+
+impl a b = (not a) || b
+
+equiv:: String -> String -> Bool
+equiv a b
+		| a == b = True
+		|otherwise = False
+
+{-
+A funcao square esta implementada e eleva ao quadrado um determinado numero
+-}
+square x = x*x
+
+{-
+- Implemente a funcao potencia, que retorna o resultado de x elevado a y 
+-}
+pow x y = undefined
+
+
+{-
+- Implemente a funcao fatorial que calcula o fatorial de um numero 
+-}
+fatorial x = undefined
+
+{-
+- Determina se um numero eh primo ou nao. Preocupe-se apenas em resolver o problema.
+- Nao precisa usar conhecimentos mais sofisticados da teoria dos numeros. Voce pode trabalhar com listas.
+-}
+isPrime x = undefined
+
+{-
+- Calcula um termo da sequencia de Fibonnacci. Voce pode trabalhar com listas. 
+-}
+fib x = undefined
+
+{-
+- Calcula um MDC de dois numeros usando o algoritmo de Euclides. 
+-}
+mdc x y = undefined
+
+{-
+- Calcula um MMC de dois numeros. 
+-}
+mmc x y = undefined
+
+{-
+- Determina se dois numeros inteiros positivos sao co-primos. Dois numeros sao co-primos se 
+- o mdc deles for igual a 1. Ex: coprimo 35 64 = True 
+-}
+coprimo x y = undefined
+
+{-
+- Calcula a conjectura de Goldbach, que diz que um numero par maior que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
+-}
+goldbach x = undefined
